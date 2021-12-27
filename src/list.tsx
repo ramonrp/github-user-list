9512dfc073a7790aa29cd2510126f21244e4bcf3
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-27 13:29:31 +0100

    feature: search organization members

--- src/list.tsx
+++ src/list.tsx
@@ -7,18 +7,36 @@ interface MemberEntity {
   avatar_url: string;
 }
 
+const getMembers = (organization) => {
+  return fetch(`https://api.github.com/orgs/${organization}/members`).then(
+    (response) => response.json()
+  );
+};
 export const ListPage: React.FC = () => {
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
+  const [inputOrganization, setInputOrganization] = React.useState("lemoncode");
 
   React.useEffect(() => {
-    fetch(`https://api.github.com/orgs/lemoncode/members`)
-      .then((response) => response.json())
-      .then((json) => setMembers(json));
+    getMembers(inputOrganization).then(setMembers);
   }, []);
+
+  const handleChange = (e) => {
+    setInputOrganization(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMembers(inputOrganization).then(setMembers);
+  };
 
   return (
     <>
       <h2>Hello from List page</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Organization</label>
+        <input onChange={handleChange} value={inputOrganization}></input>
+        <button>Search</button>
+      </form>
       <table className="table">
         <thead>
           <tr>
